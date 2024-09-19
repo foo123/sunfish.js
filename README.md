f2eaa159ddd4663c5f6b31d8f2db0145bde345da
Nikos M <workingclasscode@gmail.com>
2024-09-19 12:44:45 +0300

    completed port, added (max)depth uci option

--- README.md
+++ README.md
@@ -3,5 +3,3 @@
 JavaScript port of [Sunfish Python Chess Engine](https://github.com/thomasahle/sunfish)
 
 (NB: NNUE version is not scheduled to be ported)
-
-**in progress**
--- test.js
+++ test.js
@@ -5,4 +5,4 @@ const sunfish = require('./sunfish.js');
 console.log(sunfish.engine('uci'));
 console.log(sunfish.engine('isready'));
 console.log(sunfish.engine('position startpos moves e2e4'));
-console.log(sunfish.engine('go depth 2'));
+console.log(sunfish.engine('go depth 4'));
